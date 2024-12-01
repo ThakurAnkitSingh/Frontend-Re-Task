bfe7f139165b5748722427510913b24551aea2ce
Ankit <ankit.singh@leverageedu.com>
2024-12-01 18:34:08 +0530

    url changed

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -1,3 +1,4 @@
-const api = `https://frontend-reuion-task.onrender.com/api`;
+// const api = `https://frontend-reuion-task.onrender.com/api`;
+const api = `https://frontend-reunion-task.vercel.app/api`;
 
 export default api;
