1ec60d00846897c0167c8f461b749c6ceb0afc38
Ankit Singh <111516892+ThakurAnkitSingh@users.noreply.github.com>
2024-12-01 19:55:46 +0530

    Update 2 api.ts

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -1,4 +1,4 @@
 // const api = `https://frontend-reuion-task.onrender.com/api`;
-const api = `https://frontend-reunion-tasks.onrender.com/api`;
+const api = `https://frontend-reunion-task.onrender.com/api`;
 
 export default api;
