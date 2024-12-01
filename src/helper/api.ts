ff5ce726b683cd44192b4b6df7d6675cfdbac8d1
Ankit Singh <111516892+ThakurAnkitSingh@users.noreply.github.com>
2024-12-01 19:39:38 +0530

    Update api.ts

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -1,4 +1,4 @@
 // const api = `https://frontend-reuion-task.onrender.com/api`;
-const api = `https://frontend-reunion-task.vercel.app/api`;
+const api = `https://frontend-reunion-tasks.onrender.com/api`;
 
 export default api;
